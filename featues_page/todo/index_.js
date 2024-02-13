d1ad5391abbd5cec176ea8286198394241d7c857
tsega7664 <yeabsira.zelalem@aastustudent.edu.et>
2024-02-13 10:52:33 +0300

    check box

--- featues_page/todo/index_.js
+++ featues_page/todo/index_.js
@@ -1,38 +1,30 @@
 function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskList = document.getElementById('taskList');
-    
-    // Get the task value
-    var taskText = taskInput.value.trim();
+  var taskInput = document.getElementById('taskInput');
+  var taskList = document.getElementById('taskList');
+  var taskText = taskInput.value.trim();
+
+  if (taskText !== '') {
+    var li = document.createElement('li');
+    li.className = 'task-item';
+    li.innerHTML = `
+      <input type="checkbox" class="complete-checkbox" onclick="completeTask(this)">
+      <span>${taskText}</span>
+      <button class="delete-btn" onclick="deleteTask(this)"> X </button>
+    `;
   
-    // Check if the task is not empty
-    if (taskText !== '') {
-      // Create new list item
-      var li = document.createElement('li');
-      li.className = 'task-item';
-      li.innerHTML = `
-        <span>${taskText}</span>
-        <button class="complete-btn" onclick="completeTask(this)">✓</button>
-        <button class="delete-btn" onclick="deleteTask(this)">X</button>
-      `;
-      
-      // Append the new list item to the task list
-      taskList.appendChild(li);
-      
-      // Clear the input field after adding the task
-      taskInput.value = '';
-    } else {
-      alert('Please enter a task!');
-    }
+    taskList.appendChild(li);
+    taskInput.value = '';
+  } else {
+    alert('Please enter a task!');
   }
-  
-  function deleteTask(element) {
-    // Remove the parent list item of the clicked delete button
-    element.parentElement.remove();
-  }
-  
-  function completeTask(element) {
-    var taskItem = element.parentElement;
-    taskItem.classList.toggle('completed');
-  }
-  
+}
+
+function deleteTask(element) {
+
+  element.parentElement.remove();
+}
+
+function completeTask(element) {
+  var taskItem = element.parentElement;
+  taskItem.classList.toggle('completed');
+}
